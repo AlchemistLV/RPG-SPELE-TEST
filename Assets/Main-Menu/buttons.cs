24e64fca833cbe9bb5ff373affe2c26335848e0c
Matīss Pēčs <pecs96@gmail.com>
2017-05-20 22:23:47 +0300

    Main-Menu
    Working Main-Menu
    Deleted tons of not used Assets

---
+++ Assets/Main-Menu/buttons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour {
+    public void NewGameBtn(string newGame)
+    {
+        SceneManager.LoadScene(newGame);
+    }
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+}
